f15b354ceea9a7e3ca74e935e09cefe6c0644d63
olafkotur <olafkotur97@gmail.com>
2019-10-18 13:04:00 +0100

    Basic reading from serial port to access microbit data

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,8 @@
 module server
 
 go 1.12
+
+require (
+	github.com/argandas/serial v0.0.0-20160316175758-889a5ad85462
+	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07
+)
--- server/server.go
+++ server/server.go
@@ -1,16 +1,32 @@
 package main
- 
+
 import (
-    "log"
-    "net/http"
+	"github.com/argandas/serial"
 )
- 
+
+const PORT = "8080"
+const SERIAL_PORT_NAME = "/dev/cu.usbmodem141302"
+const SERIAL_PORT_BAUD = 115200
+
 func main() {
-    port := "8080"
+	// port := "8080"
 
-    fs := http.FileServer(http.Dir("../webapp/build"))
-    http.Handle("/", fs)
+	// fs := http.FileServer(http.Dir("../webapp/build"))
+	// http.Handle("/", fs)
 
-    log.Println("Listening on " + port + "...")
-    http.ListenAndServe(":" + port, nil)
+	// log.Println("Listening on " + port + "...")
+	// http.ListenAndServe(":"+port, nil)
+	readSerial()
+}
+
+func readSerial() {
+	sp := serial.New()
+	err := sp.Open(SERIAL_PORT_NAME, SERIAL_PORT_BAUD)
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		sp.ReadLine()
+	}
 }
