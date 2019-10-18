34d4e6bfc32d1435892b55cdf8065cc0f3828174
olafkotur <olafkotur97@gmail.com>
2019-10-18 21:02:38 +0100

    Server is able to concurrently host the webapp and read from serial port

--- server/server.go
+++ server/server.go
@@ -9,19 +9,20 @@ import (
 
 const PORT = "8080"
 const WEB_BUILD_PATH = "../webapp/build"
-const SERIAL_PORT_NAME = "/dev/cu.usbmodem141202"
+const SERIAL_PORT_NAME = "/dev/cu.usbmodem141302"
 const SERIAL_PORT_BAUD = 115200
 
 func main() {
+	startServer()
 	readSerial()
 }
 
-func start() {
+func startServer() {
 	var fs = http.FileServer(http.Dir(WEB_BUILD_PATH))
 	http.Handle("/", fs)
 
 	log.Println("Listening on " + PORT + "...")
-	http.ListenAndServe(":"+PORT, nil)
+	go http.ListenAndServe(":"+PORT, nil)
 }
 
 func readSerial() {
