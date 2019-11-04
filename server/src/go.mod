d77ec5656cdb3d029c9e325d033f7f7361cbf8d1
olafkotur <olafkotur97@gmail.com>
2019-11-04 20:02:05 +0000

    Server is able to serve both static and restful services

--- server/src/go.mod
+++ server/src/go.mod
@@ -2,4 +2,7 @@ module main
 
 go 1.13
 
-require github.com/joho/godotenv v1.3.0
+require (
+	github.com/gorilla/mux v1.7.3
+	github.com/joho/godotenv v1.3.0
+)
--- server/src/main.go
+++ server/src/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 )
 
@@ -16,16 +19,41 @@ func main() {
 	}
 
 	// Enviornment variables
-	SERVER_PORT := os.Getenv("SERVER_PORT")
+	STATIC_SERVER_PORT := os.Getenv("STATIC_SERVER_PORT")
+	REST_SERVER_PORT := os.Getenv("REST_SERVER_PORT")
 	WEB_BUILD_PATH := os.Getenv("WEB_BUILD_PATH")
 
-	go startServer(SERVER_PORT, WEB_BUILD_PATH)
+	go serveStatic(STATIC_SERVER_PORT, WEB_BUILD_PATH)
+	serveRestful(REST_SERVER_PORT)
 }
 
-func startServer(port, path string) {
-	var fs = http.FileServer(http.Dir(path))
+func serveStatic(port, path string) {
+	fs := http.FileServer(http.Dir(path))
 	http.Handle("/", fs)
 
-	log.Printf("Listening on %s...\n\n", port)
+	log.Printf("Serving static on port %s...\n", port)
 	http.ListenAndServe(":"+port, nil)
+}
+
+func serveRestful(port string) {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/api", sayHello)
+
+	log.Printf("Serving restful on port %s...\n", port)
+	http.ListenAndServe(":"+port, router)
+
+}
+
+func sayHello(writer http.ResponseWriter, request *http.Request) {
+	fmt.Println("Hello")
+	type Data struct {
+		Message string `json:"message"`
+	}
+
+	data := Data{"Hello World"}
+	response, _ := json.Marshal(data)
+
+	fmt.Println("Yuppp")
+	writer.Header().Set("Content-Type", "application/json")
+	writer.Write(response)
 }
