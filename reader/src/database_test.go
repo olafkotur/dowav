7f557f618bdf21bc99d799d1e45f257f4198ab3a
lwjace <44038890+lwjace@users.noreply.github.com>
2019-11-13 16:30:17 +0000

    add test and make easier to use funcs

---
+++ reader/src/database_test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"testing"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func TestQueryHourlyData(t *testing.T) {
+	value := queryHourlyData("TestDB", 0)
+	if len(value) == 0 {
+		t.Error("Expected the last value of the TestDB talbe instead of 0")
+	}
+}
+
+func TestQueryMaxData(t *testing.T) {
+	value := queryMaxData("TestDB", "temp", "0")
+	if value != 300 {
+		t.Error("Expected to be 300 for max temp in TestDB")
+	}
+}
+
+func TestQueryMinData(t *testing.T) {
+	value := queryMinData("TestDB", "temp", "0")
+	if value != 0 {
+		t.Error("Expected to be 0 for min temp in TestDB")
+	}
+}
+
+func TestQueryAvgData(t *testing.T) {
+	value := queryAvgData("TestDB", "temp", "150")
+	if value != 150 {
+		t.Error("Expected to be 150 for Average temp in TestDB")
+	}
+}
+
+func TestInsertData(t *testing.T) {
+	intsertData("TestIDB", 10, 10, 10, 10)
+	th := time.Now()
+	tH := th.Hour()
+	value := queryHourlyData("TestIDB", tH)
+	if value != 10 {
+		t.Log(value)
+		t.Error("Expected to be 10 as last value was 10")
+	}
+}
