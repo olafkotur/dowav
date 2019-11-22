de46046e61bf24197860e896f2a0a23245008d0a
Alex Dhawan <alex@dhawan.me.uk>
2019-11-22 01:29:19 +0000

    changing serial data format as per new spec. fix bug of user reporting themselves to be in watering can

--- sensors/source/main.cpp
+++ sensors/source/main.cpp
@@ -3,6 +3,8 @@
 
 MicroBit uBit;
 int zoneId = 0;
+int deviceID = 0;
+int oldDeviceID = deviceID;//Used to detect changes for display update
 int signalStrength = -128;
 char currentLocation = '0';
 int userLocationLastUpdateTime = 0;
@@ -76,7 +78,30 @@ int getMoistureLevel() {
 int temperature = getTemperature();
 int moisture = getMoistureLevel();
 int light = getLightLevel();
-int water = getWaterLevel();
+int waterLevel = getWaterLevel();
+int accelerometerX = getAccelorometerX();
+int accelerometerY = getAccelorometerY();
+int accelerometerZ = getAccelorometerZ();
+
+void setDeviceId(){
+  if(zoneId == 3 || zoneId == 1){
+    //User or watering can
+    if(uBit.io.P0.isTouched()==true){
+      deviceID = 0;
+    } else if(uBit.io.P1.isTouched()==true){
+      deviceID = 1;
+    } else if(uBit.io.P2.isTouched()==true){
+      deviceID = 2;
+    }
+
+    if(deviceID != oldDeviceID){
+      ManagedString deivceIDmsg("ID:");
+      uBit.display.scrollAsync(deivceIDmsg + deviceID);
+      oldDeviceID = deviceID;
+    }
+  }
+
+}
 
 void printzoneId() {
   if(on == 0){
@@ -84,9 +109,13 @@ void printzoneId() {
   } else if (on == 1){
     uBit.display.setBrightness(255);
   }
-  if (zoneId > 2) {
-      uBit.display.print(zoneId-2);
+
+  if (zoneId > 3) {
+      uBit.display.print(zoneId-3);
   }
+  else if (zoneId == 3) {
+      uBit.display.printAsync("W");
+    }
   else if (zoneId == 2) {
     uBit.display.printAsync("R");
   }
@@ -99,7 +128,7 @@ void printzoneId() {
 }
 
 void onButtonEvent(MicroBitEvent e) {
-  int maxZones = 5;
+  int maxZones = 6;
   if (e.source == MICROBIT_ID_BUTTON_A && zoneId > 0) {
     zoneId--;
   }
@@ -118,7 +147,7 @@ void onButtonEvent(MicroBitEvent e) {
 
 void sendMessage(ManagedString msg) {
   uBit.radio.datagram.send(msg);
-  uBit.serial.printf("S%s\r\n", msg.toCharArray());
+  uBit.serial.printf("%s\r\n", msg.toCharArray());
 }
 
 void receiveMessage(MicroBitEvent) {
@@ -132,29 +161,31 @@ void receiveMessage(MicroBitEvent) {
       if (msg[0] == 'U') {
         currentLocation = msg[1];
         userLocationLastUpdateTime = currentTime;
-      } else {
-        if(currentTime-userLocationLastUpdateTime<10){
-          uBit.serial.printf("R%s %c\r\n", msg, currentLocation);
+      }// else {
+        /*if(currentTime-userLocationLastUpdateTime<10){
+          uBit.serial.printf("%s %c\r\n", msg, currentLocation);
         } else {
           //No user recived for more then 10 seconds - so send 0 for location
-          uBit.serial.printf("R%s 0\r\n", msg);
-        }
-      }
+          uBit.serial.printf("%s 0\r\n", msg);
+        }*/
+      uBit.serial.printf("%s\r\n", msg);
+      //}
     }
 
   //User
-    if(msg[0] != 'U' && zoneId == 1) {
-      if (msg[0] == currentLocation) {
+    if(msg[1] != 'U' && msg[1] != 'W' && zoneId == 1) {
+      if (msg[1] == currentLocation) {
         signalStrength = uBit.radio.getRSSI();
       }
       if (uBit.radio.getRSSI() > signalStrength) {
         signalStrength = uBit.radio.getRSSI();
-        currentLocation = msg[0];
+        currentLocation = msg[1];
       }
-      ManagedString prefix("U");
-      ManagedString zone(currentLocation);
-      uBit.radio.datagram.send(prefix + zone);
-      uBit.serial.printf("%s\r\n",zone.toCharArray());
+      //ManagedString prefix("U");
+      //ManagedString zone(currentLocation);
+      //uBit.radio.datagram.send(prefix + zone);
+      //uBit.serial.printf("%s\r\n",zone.toCharArray());
+      sendMessage(ManagedString('U') + ManagedString(deviceID) + ManagedString(' ') + ManagedString(currentLocation));
     }
   }
 }
@@ -174,22 +205,36 @@ int main() {
 
   while(true) {
     if (on==1){
-      // Senders
-      if (zoneId-2 > 0) {
+      // Sensors
+      if (zoneId > 3) {
         temperature = getTemperature();
         moisture = getMoistureLevel();
         light = getLightLevel();
-        water = getWaterLevel();
+        waterLevel = getWaterLevel();
 
-        sendMessage(ManagedString(ManagedString(zoneId-2) + ManagedString(' ') +
+        sendMessage(ManagedString(ManagedString('R') +
+                                  ManagedString(zoneId-3) + ManagedString(' ') +
                                   ManagedString(temperature) + ManagedString(' ') +
                                   ManagedString(moisture) + ManagedString(' ') +
                                   ManagedString(light) + ManagedString(' ') +
-                                  ManagedString(water)));
+                                  ManagedString(waterLevel)));
+      } else if (zoneId == 3){
+        //Watering can
+        waterLevel = getWaterLevel();
+        accelerometerX = getAccelorometerX();
+        accelerometerY = getAccelorometerY();
+        accelerometerZ = getAccelorometerZ();
+
+        sendMessage(ManagedString(ManagedString('W') + ManagedString(deviceID) + ManagedString(' ') +
+                    ManagedString(accelerometerX) + ManagedString(' ') +
+                    ManagedString(accelerometerY) + ManagedString(' ') +
+                    ManagedString(accelerometerZ) + ManagedString(' ') +
+                    ManagedString(waterLevel)));
       }
     }
     currentTime = currentTime + 1;
     uBit.sleep(1000);
     printzoneId();
+    setDeviceId();
   }
 }
